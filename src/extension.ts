a10f67505dd0fd9839ff446b964e3927d50b835a
Murtada Altarouti <murtada.altarouti@gmail.com>
2024-02-11 19:42:51 +0300

    Added performance measurment

--- src/extension.ts
+++ src/extension.ts
@@ -2,9 +2,13 @@ import * as vscode from 'vscode';
 import { WordTeleporterExtension as WordTeleporter } from './word_teleporter/word_teleporter';
 
 export function activate(context: vscode.ExtensionContext) {
+	const wt = new WordTeleporter();
 	let wordTeleporter = vscode.commands.registerCommand('teleporter.wordTeleporter', () => {
-		const wt = new WordTeleporter();
+		const startTime = performance.now();
 		wt.run();
+		const endTime = performance.now();
+		const executionTimeInSeconds = (endTime - startTime) / 1000;
+		vscode.window.showInformationMessage(`${executionTimeInSeconds.toString()} seconds`);
 	});
 	context.subscriptions.push(wordTeleporter);
 }
